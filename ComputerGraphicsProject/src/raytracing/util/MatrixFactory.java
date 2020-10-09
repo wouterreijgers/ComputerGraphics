4a69a78a261493b245305066a15094ffb9c02488
Wouter Reijgersberg <wouter.reijgersberg@student.uantwerpen.be>
2020-10-09 10:15:50 +0200

    first part of factory is added

--- ComputerGraphicsProject/src/raytracing/util/MatrixFactory.java
+++ ComputerGraphicsProject/src/raytracing/util/MatrixFactory.java
@@ -8,4 +8,41 @@ public class MatrixFactory {
                .change(3, 2, z);
     }
 
+    public Matrix scalingMatrix(int x, int y, int z){
+        return new Matrix()
+                .change(0, 0, x)
+                .change(1, 1, y)
+                .change(2, 2, z);
+
+    }
+
+    public Matrix rotationMatrix(String axis, double angle){
+        double cos = Math.cos(angle);
+        double sin = Math.sin(angle);
+        switch (axis)
+        {
+            case "X":
+                return new Matrix()
+                        .change(1, 1, cos)
+                        .change(2, 1, -sin)
+                        .change(2, 2, cos)
+                        .change(2, 1, cos);
+                break;
+            case "Y":
+                return new Matrix();
+                break;
+            case "Z":
+                return new Matrix();
+                break;
+            default:
+                return new Matrix();
+        }
+
+        return new Matrix()
+                .change(0, 0, x)
+                .change(1, 1, y)
+                .change(2, 2, z);
+
+    }
+
 }
--- ComputerGraphicsProject/src/raytracing/util/testing/FactoryTest.java
+++ ComputerGraphicsProject/src/raytracing/util/testing/FactoryTest.java
@@ -21,6 +21,16 @@ class FactoryTest {
         assertEquals(true, compareMatrices(translation, new Matrix(expected)));
     }
 
+    void testScaling() {
+        double[][] expected = {
+                {3, 0, 0, 0},
+                {0, 4, 0, 0},
+                {0, 0, 2, 0},
+                {0, 0, 0, 1}};
+        Matrix translation = new MatrixFactory().scalingMatrix(3, 4, 2);
+        assertEquals(true, compareMatrices(translation, new Matrix(expected)));
+    }
+
 
     public Boolean compareMatrices(Matrix a, Matrix b) {
         for(int i=0;i<4;i++){
